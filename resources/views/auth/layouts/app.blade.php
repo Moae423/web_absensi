af44b0446e2a8a7f3c1bda6e6b94038b859e9e91
Daffa Rihadatul Fairus <hanura423@gmail.com>
2025-01-15 23:13:03 +0700

    FE: Bikin Notif Error Pada Login Page

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="light">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/js/app.js', )
+    @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     <title>{{ $title }}</title>
 </head>
+
 <body>
+
     <main class="min-h-screen bg-darkBlue">
         @yield('content')
     </main>
 </body>
 <script src="./node_modules/preline/dist/preline.js"></script>
+
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,19 @@
 @extends('auth.layouts.app')
 
 @section('content')
+    @if ($errors->any())
+        <div role="alert" class="alert alert-error">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>
+                <strong>Error!</strong>
+                {{ $errors->first() }}
+            </span>
+        </div>
+    @endif
+
     <div class="hero min-h-screen  max-w-screen-lg mx-auto font-satoshi">
         <div class="hero-content flex-col lg:flex-row-reverse">
             <div class="text-center lg:text-left">
